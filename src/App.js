06436980ab53cbe318928bc38cc3122a8accf9a7
md.Shahriar Ovi <83135100+SHAHRIAR-SO@users.noreply.github.com>
2022-04-21 02:39:30 +0600

    function return

--- src/App.js
+++ src/App.js
@@ -5,21 +5,33 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+
+        {/* <p>Try to learn Reactjs</p> */}
+        <Person></Person>
+        
+        <Person></Person>
+       
+        <Person></Person>
       </header>
     </div>
   );
+}
+function Person() {
+  const personstyle = {
+   border : '2px solid yellow',
+   margin : '10 px'
+  }
+  return (
+
+    <div style={{border: '5px solid yellow',margin: '10px'}}>
+      <h1>Md.Shahriar ovi</h1>
+      <h3>Learn React js Very Carefully</h3>
+    </div>
+  )
+
+
+
 }
 
 export default App;
